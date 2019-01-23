87360ea6dfdd55a0f8b76fcf084ee534490cda92
Bryan <bryan_kenis@hotmail.com>
2019-01-23 11:18:34 +0100

    Finis

--- GitExercice/GitExercice/Form1.Designer.cs
+++ GitExercice/GitExercice/Form1.Designer.cs
@@ -28,21 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lorbtn = new System.Windows.Forms.Button();
+            this.starwarsbtn = new System.Windows.Forms.Button();
+            this.wowbtn = new System.Windows.Forms.Button();
+            this.narniabtn = new System.Windows.Forms.Button();
+            this.Univers = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            
+            // 
+            // lorbtn
+            // 
+            this.lorbtn.Location = new System.Drawing.Point(413, 320);
+            this.lorbtn.Name = "lorbtn";
+            this.lorbtn.Size = new System.Drawing.Size(208, 77);
+            this.lorbtn.TabIndex = 0;
+            this.lorbtn.Text = "Le Seigneur Des Anneaux";
+            this.lorbtn.UseVisualStyleBackColor = true;
+            this.lorbtn.Click += new System.EventHandler(this.lorbtn_Click);
+            // 
+            // starwarsbtn
+            // 
+            this.starwarsbtn.Location = new System.Drawing.Point(89, 320);
+            this.starwarsbtn.Name = "starwarsbtn";
+            this.starwarsbtn.Size = new System.Drawing.Size(207, 78);
+            this.starwarsbtn.TabIndex = 1;
+            this.starwarsbtn.Text = "Star Wars";
+            this.starwarsbtn.UseVisualStyleBackColor = true;
+            this.starwarsbtn.Click += new System.EventHandler(this.starwarsbtn_Click);
+            // 
+            // wowbtn
+            // 
+            this.wowbtn.Location = new System.Drawing.Point(413, 180);
+            this.wowbtn.Name = "wowbtn";
+            this.wowbtn.Size = new System.Drawing.Size(205, 75);
+            this.wowbtn.TabIndex = 2;
+            this.wowbtn.Text = "World Of Warcraft";
+            this.wowbtn.UseVisualStyleBackColor = true;
+            this.wowbtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // narniabtn
+            // 
+            this.narniabtn.Location = new System.Drawing.Point(89, 183);
+            this.narniabtn.Name = "narniabtn";
+            this.narniabtn.Size = new System.Drawing.Size(207, 72);
+            this.narniabtn.TabIndex = 3;
+            this.narniabtn.Text = "Narnia";
+            this.narniabtn.UseVisualStyleBackColor = true;
+            this.narniabtn.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Univers
+            // 
+            this.Univers.AutoSize = true;
+            this.Univers.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Univers.ForeColor = System.Drawing.Color.DarkRed;
+            this.Univers.Location = new System.Drawing.Point(12, 94);
+            this.Univers.Name = "Univers";
+            this.Univers.Size = new System.Drawing.Size(476, 37);
+            this.Univers.TabIndex = 4;
+            this.Univers.Text = "Quel est votre Univers préférer ?";
+            this.Univers.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Univers);
+            this.Controls.Add(this.narniabtn);
+            this.Controls.Add(this.wowbtn);
+            this.Controls.Add(this.starwarsbtn);
+            this.Controls.Add(this.lorbtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button lorbtn;
+        private System.Windows.Forms.Button starwarsbtn;
+        private System.Windows.Forms.Button wowbtn;
+        private System.Windows.Forms.Button narniabtn;
+        private System.Windows.Forms.Label Univers;
     }
 }
 
--- GitExercice/GitExercice/Form1.cs
+++ GitExercice/GitExercice/Form1.cs
@@ -12,9 +12,44 @@ namespace GitExercice
 {
     public partial class Form1 : Form
     {
+        int starwars = 0;
+        int narnia = 0;
+        int wow = 0;
+        int lor = 0;
         public Form1()
         {
             InitializeComponent();
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK, Merci pour votre participation");
+            narnia++;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK, Merci pour votre participation");
+            wow++;
+        }
+
+        private void starwarsbtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK, Merci pour votre participation");
+            starwars++;
+        }
+
+        private void lorbtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK, Merci pour votre participation");
+            lor++;
         }
     }
 }
