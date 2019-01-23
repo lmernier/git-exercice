78af15066419d4cb76f0ccb473276a94530e18aa
mdaniel1 <daniel.martin1828@gmail.com>
2019-01-23 10:44:43 +0100

    Fixed hugo's mistakes

--- GitExercice/GitExercice/Form1.cs
+++ GitExercice/GitExercice/Form1.cs
@@ -16,13 +16,6 @@ namespace GitExercice
         {
             InitializeComponent();
         }
-        Form1 me = new Form1();
-        private void Btn_GoToHK_Click(object sender, EventArgs e)
-        {
-            HK hk = new HK();
-            hk.Show();
-            me.Close();
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
